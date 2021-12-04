a05cfb34d225ff667364908369a4e1c9541bbebc
laercioferracini <laercio_ferracini@yahoo.com.br>
2021-12-04 20:50:54 -0300

    method @Autowired example

--- src/main/java/br/com/ferracini/service/SpeakerServiceImpl.java
+++ src/main/java/br/com/ferracini/service/SpeakerServiceImpl.java
@@ -3,23 +3,32 @@ package br.com.ferracini.service;
 import br.com.ferracini.model.Speaker;
 import br.com.ferracini.repository.HibernateSpeakRepositoryImpl;
 import br.com.ferracini.repository.SpeakRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 public class SpeakerServiceImpl implements SpeakerService {
-
+    Logger logger = Logger.getLogger(SpeakerServiceImpl.class.getName());
     private SpeakRepository repository;
 
+    public SpeakerServiceImpl() {
+        logger.info("SpeakerServiceImpl no args constructor");
+    }
+
     public SpeakerServiceImpl(SpeakRepository repository) {
+        logger.info("SpeakerServiceImpl repository constructor");
         this.repository = repository;
     }
 
     @Override
-    public List<Speaker> findAll(){
+    public List<Speaker> findAll() {
         return repository.findAll();
     }
 
+    @Autowired
     public void setRepository(SpeakRepository repository) {
+        logger.info("SpeakerServiceImpl setter");
         this.repository = repository;
     }
 }
--- src/main/java/br/com/ferracini/springfundamentals/SpringFundamentalsApplication.java
+++ src/main/java/br/com/ferracini/springfundamentals/SpringFundamentalsApplication.java
@@ -7,19 +7,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class SpringFundamentalsApplication {
+    static Logger logger = Logger.getLogger(SpringFundamentalsApplication.class.getName());
 
-	public static void main(String[] args) {
-		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
 
-		SpeakerService service = applicationContext.getBean("speakerService", SpeakerService.class);
-		System.out.println(service);
-		System.out.println(service.findAll());
+        SpeakerService service = applicationContext.getBean("speakerService", SpeakerService.class);
+        logger.info(String.valueOf(service));
+        logger.info(String.valueOf(service.findAll()));
 
-		SpeakerService service2 = applicationContext.getBean("speakerService", SpeakerService.class);
-		System.out.println(service2);
-		//SpringApplication.run(SpringFundamentalsApplication.class, args);
-	}
+        SpeakerService service2 = applicationContext.getBean("speakerService", SpeakerService.class);
+        logger.info(String.valueOf(service2));
+        //SpringApplication.run(SpringFundamentalsApplication.class, args);
+    }
 
 }
