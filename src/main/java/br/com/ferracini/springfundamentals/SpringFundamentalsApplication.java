57e3dcaf536fbfc9d46986a3bf8e64bb761e6bfd
laercioferracini <laercio_ferracini@yahoo.com.br>
2021-12-04 22:21:12 -0300

    Adding logging with DEBUG mode on spring
    The output can be used to see how the spring are loading the configuration

--- src/main/java/br/com/ferracini/springfundamentals/SpringFundamentalsApplication.java
+++ src/main/java/br/com/ferracini/springfundamentals/SpringFundamentalsApplication.java
@@ -3,6 +3,7 @@ package br.com.ferracini.springfundamentals;
 import br.com.ferracini.service.SpeakerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -13,6 +14,7 @@ public class SpringFundamentalsApplication {
     static Logger logger = LoggerFactory.getLogger(SpringFundamentalsApplication.class.getName());
 
     public static void main(String[] args) {
+        SpringApplication.run(SpringFundamentalsApplication.class, args);
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
 
         SpeakerService service = applicationContext.getBean("speakerService", SpeakerService.class);
@@ -21,7 +23,6 @@ public class SpringFundamentalsApplication {
 
         SpeakerService service2 = applicationContext.getBean("speakerService", SpeakerService.class);
         logger.info(String.valueOf(service2));
-        //SpringApplication.run(SpringFundamentalsApplication.class, args);
     }
 
 }
