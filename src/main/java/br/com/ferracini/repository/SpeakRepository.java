151ebe10a5604b2663b8a10e34f51518bf2e88dd
laercioferracini <laercio_ferracini@yahoo.com.br>
2021-12-08 10:18:53 -0300

    Fixing class name typo in SpeakerRepository

--- src/main/java/br/com/ferracini/repository/SpeakRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.ferracini.repository;
-
-import br.com.ferracini.model.Speaker;
-
-import java.util.List;
-
-public interface SpeakRepository {
-    List<Speaker> findAll();
-}
--- src/main/java/br/com/ferracini/service/SpeakerServiceImpl.java
+++ src/main/java/br/com/ferracini/service/SpeakerServiceImpl.java
@@ -1,7 +1,7 @@
 package br.com.ferracini.service;
 
 import br.com.ferracini.model.Speaker;
-import br.com.ferracini.repository.SpeakRepository;
+import br.com.ferracini.repository.SpeakerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +14,7 @@ public class SpeakerServiceImpl implements SpeakerService {
     Logger logger = LoggerFactory.getLogger(SpeakerServiceImpl.class);
 
     @Autowired
-    private SpeakRepository repository;
+    private SpeakerRepository repository;
 
     public SpeakerServiceImpl() {
         logger.info("SpeakerServiceImpl no args constructor");
