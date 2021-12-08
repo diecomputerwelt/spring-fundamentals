402fb2988964a46885c5c999da4f54bc139bb1e4
laercioferracini <laercio_ferracini@yahoo.com.br>
2021-12-08 11:35:16 -0300

    Creating Session and Speaker Controllers

---
+++ src/main/java/br/com/ferracini/controller/SpeakerController.java
@@ -0,0 +1,49 @@
+package br.com.ferracini.controller;
+
+import br.com.ferracini.model.Speaker;
+import br.com.ferracini.repository.SpeakerRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/speakers")
+public class SpeakerController {
+
+    @Autowired
+    private SpeakerRepository speakerRepository;
+
+    @GetMapping
+    public List<Speaker> list() {
+        return speakerRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Speaker get(@PathVariable Long id) {
+        return speakerRepository.getById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Speaker create(@RequestBody final Speaker speaker) {
+        return speakerRepository.saveAndFlush(speaker);
+    }
+
+    @DeleteMapping(value = "{id}")
+    public void delete(@PathVariable Long id) {
+        //Need to check children records before deleting
+        speakerRepository.deleteById(id);
+    }
+
+    @PutMapping(value = "{id}")
+    public Speaker update(@PathVariable Long id, @RequestBody Speaker speaker) {
+        //TODO: Add validation that all attributes are passed id, otherwise return 400 Bad Request
+        Speaker existingSpeaker = speakerRepository.getById(id);
+        BeanUtils.copyProperties(speaker, existingSpeaker, "session_id");
+        return speakerRepository.saveAndFlush(existingSpeaker);
+    }
+}
--- src/main/java/br/com/ferracini/repository/SpeakerRepository.java
+++ src/main/java/br/com/ferracini/repository/SpeakerRepository.java
@@ -3,8 +3,6 @@ package br.com.ferracini.repository;
 import br.com.ferracini.model.Speaker;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface SpeakerRepository extends JpaRepository<Speaker, Long> {
-    List<Speaker> findAll();
+
 }
