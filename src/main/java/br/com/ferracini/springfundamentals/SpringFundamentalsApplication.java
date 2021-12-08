c7c652675d013d1d246efeaccc5f8125b334cb13
laercioferracini <laercio_ferracini@yahoo.com.br>
2021-12-08 10:19:08 -0300

    Removing old code

--- src/main/java/br/com/ferracini/springfundamentals/SpringFundamentalsApplication.java
+++ src/main/java/br/com/ferracini/springfundamentals/SpringFundamentalsApplication.java
@@ -1,13 +1,9 @@
 package br.com.ferracini.springfundamentals;
 
-import br.com.ferracini.config.AppConfig;
-import br.com.ferracini.service.SpeakerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 
 @SpringBootApplication
@@ -16,14 +12,6 @@ public class SpringFundamentalsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringFundamentalsApplication.class, args);
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-
-        SpeakerService service = applicationContext.getBean("speakerService", SpeakerService.class);
-        logger.info(String.valueOf(service));
-        logger.info(String.valueOf(service.findAll()));
-
-        SpeakerService service2 = applicationContext.getBean("speakerService", SpeakerService.class);
-        logger.info(String.valueOf(service2));
     }
 
 }
