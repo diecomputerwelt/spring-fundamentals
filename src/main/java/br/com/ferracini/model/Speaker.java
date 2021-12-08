a305fa4e1f3131f9e60636286c42903cdf8a9993
laercioferracini <laercio_ferracini@yahoo.com.br>
2021-12-08 09:41:26 -0300

    Creating Speaker Entity using Column annotation

--- src/main/java/br/com/ferracini/model/Speaker.java
+++ src/main/java/br/com/ferracini/model/Speaker.java
@@ -1,8 +1,32 @@
 package br.com.ferracini.model;
 
+import javax.persistence.*;
+
+@Entity(name = "speakers")
 public class Speaker {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "speaker_id")
+    private Long speakerId;
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "company")
+    private String company;
+    @Column(name = "speaker_bio")
+    private String speakerBio;
+
+    public Long getSpeakerId() {
+        return speakerId;
+    }
+
+    public void setSpeakerId(Long speakerId) {
+        this.speakerId = speakerId;
+    }
 
     public String getFirstName() {
         return firstName;
@@ -20,11 +44,39 @@ public class Speaker {
         this.lastName = lastName;
     }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getSpeakerBio() {
+        return speakerBio;
+    }
+
+    public void setSpeakerBio(String speakerBio) {
+        this.speakerBio = speakerBio;
+    }
+
     @Override
     public String toString() {
         return "Speaker{" +
-                "firstName='" + firstName + '\'' +
+                "speakerId=" + speakerId +
+                ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
+                ", title='" + title + '\'' +
+                ", company='" + company + '\'' +
+                ", speakerBio='" + speakerBio + '\'' +
                 '}';
     }
 }
