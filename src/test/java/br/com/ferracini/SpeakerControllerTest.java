f193dd23bcc9422324a9c61ea28dcd3d9004ba2d
laercioferracini <laercio_ferracini@yahoo.com.br>
2022-04-03 12:48:09 -0300

    DT-3 Completed unit tests for SpeakerController

--- src/test/java/br/com/ferracini/SpeakerControllerTest.java
+++ src/test/java/br/com/ferracini/SpeakerControllerTest.java
@@ -3,19 +3,18 @@ package br.com.ferracini;
 import br.com.ferracini.controller.SpeakerController;
 import br.com.ferracini.model.Speaker;
 import br.com.ferracini.repository.SpeakerRepository;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.*;
 import org.mockito.Mock;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 import java.util.Random;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.openMocks;
 
 @DisplayName(value = "Speaker controller test")
@@ -26,11 +25,19 @@ class SpeakerControllerTest {
 
     @Mock
     private SpeakerRepository speakerRepository;
+    private Speaker speaker;
 
     @BeforeEach
     void setUp() {
         openMocks(this);
         subject = new SpeakerController(speakerRepository);
+        speaker = new Speaker();
+        speaker.setSpeakerId(1L);
+        speaker.setFirstName("Freddy");
+        speaker.setLastName("Mercury");
+        speaker.setCompany("Test company");
+        speaker.setTitle("Test title");
+        speaker.setSpeakerBio("Test bio");
     }
 
     @Order(1)
@@ -46,48 +53,68 @@ class SpeakerControllerTest {
     @Test
     @DisplayName("should return list of speakers")
     void listSpeakersTest() {
-        var speaker1 = getSpeaker();
         var speaker2 = getSpeaker();
         var speakers = new ArrayList<Speaker>();
-        speakers.add(speaker1);
+        speakers.add(speaker);
         speakers.add(speaker2);
         given(speakerRepository.findAll()).willReturn(speakers);
 
         var speakerList = subject.list();
 
-        assertThat(speakerList, contains(speaker1, speaker2));
+        assertThat(speakerList, contains(speaker, speaker2));
     }
 
     @Test
     @DisplayName(value = "should return speaker by id")
-    void returnSpeakerByIdTest(){
-        var speaker = new Speaker();
-        speaker.setSpeakerId(1L);
-        speaker.setFirstName("Rich");
-        speaker.setLastName("Blackmore");
+    void returnSpeakerByIdTest() {
+
         given(speakerRepository.findById(1L)).willReturn(Optional.of(speaker));
 
         var expectedSpeaker = subject.get(1L);
 
         assertThat(expectedSpeaker, is(speaker));
     }
-@Test
-@DisplayName(value = "should create speaker")
-void createSpeaker(){
-    var speaker = new Speaker();
-    speaker.setFirstName("Freddy");
-    speaker.setLastName("Mercury");
-    speaker.setCompany("Test company");
-    speaker.setTitle("Test title");
-    speaker.setSpeakerBio("Test bio");
 
-    given(speakerRepository.saveAndFlush(speaker)).willReturn(speaker);
+    @Test
+    @DisplayName(value = "should create speaker")
+    void createSpeaker() {
 
-    var expectedSpeaker = subject.create(speaker);
+        given(speakerRepository.saveAndFlush(speaker)).willReturn(speaker);
 
-    assertThat(expectedSpeaker.getSpeakerId(), is(1));
+        var expectedSpeaker = subject.create(speaker);
 
-}
+        assertThat(expectedSpeaker, is(speaker));
+
+    }
+
+    @Test
+    @DisplayName(value = "should update speaker")
+    void updateSpeakerTest() {
+        var id = 1L;
+        var speakerUpdated = speaker;
+        speakerUpdated.setTitle("Updated title");
+        speakerUpdated.setCompany("Updated company");
+        speakerUpdated.setSpeakerBio("Updated bio");
+
+        given(speakerRepository.findById(id)).willReturn(Optional.of(speaker));
+        given(speakerRepository.saveAndFlush(speakerUpdated)).willReturn(speakerUpdated);
+
+        var expectedSpeakerUpdated = subject.update(id, speaker);
+
+        assertThat(expectedSpeakerUpdated, is(speakerUpdated));
+    }
+
+    @Test
+    @DisplayName(value = "should delete speaker by id")
+    void shouldDeleteSpeakerByIdTest() {
+        var id = 1L;
+        doNothing().when(speakerRepository).deleteById(id);
+
+        subject.delete(id);
+
+        verify(speakerRepository, times(1)).deleteById(id);
+
+    }
 
     private Speaker getSpeaker() {
         var speaker = new Speaker();
