54733597869078ae911237b2f0943f2158ad4e65
laercioferracini <laercio_ferracini@yahoo.com.br>
2022-04-03 12:49:09 -0300

    DT-3 Reformating code

--- src/main/java/br/com/ferracini/controller/SessionsController.java
+++ src/main/java/br/com/ferracini/controller/SessionsController.java
@@ -17,29 +17,29 @@ public class SessionsController {
     private SessionRepository sessionRepository;
 
     @GetMapping
-    public List<Session> list(){
+    public List<Session> list() {
         return sessionRepository.findAll();
     }
 
     @GetMapping
     @RequestMapping("{id}")
-    public Session get(@PathVariable Long id){
+    public Session get(@PathVariable Long id) {
         return sessionRepository.findById(id).orElseThrow();
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Session create(@RequestBody final Session session){
+    public Session create(@RequestBody final Session session) {
         return sessionRepository.saveAndFlush(session);
     }
 
     @DeleteMapping(value = "{id}")
-    public void delete(@PathVariable Long id){
+    public void delete(@PathVariable Long id) {
         sessionRepository.deleteById(id);
     }
 
     @PutMapping(value = "{id}")
-    public Session update(@PathVariable Long id, @RequestBody Session session){
+    public Session update(@PathVariable Long id, @RequestBody Session session) {
         var existingSession = sessionRepository.findById(id).orElseThrow();
         BeanUtils.copyProperties(session, existingSession, "session_id");
         return sessionRepository.saveAndFlush(existingSession);
